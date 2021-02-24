7ccc16c1d3325f4e46dfa267d3c416f08ec5501a
Basile Leroy <basile.leroy.jnm@hotmail.com>
2021-02-24 17:14:30 +0100

    Removing unused variables in Nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -1,12 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react';
 
 export default function Nav() {
 
-    const [isActive, setActive] = useState()
-
     return (
         <div className="mt-5 m-5">
-            <a href="/" className="w-50 btn btn-primary active">My List</a>
+            <a href="/" className="w-50 btn btn-primary">My List</a>
             <a href="/calendar" className="w-50 btn btn-primary">My Calendar</a>
         </div>
     )
