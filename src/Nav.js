afc7a75a1959783ba927469c3003b4682ea70684
Basile Leroy <basile.leroy.jnm@hotmail.com>
2021-03-27 17:36:26 +0100

    Changing anchor tags into Links from react router dom

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Nav() {
 
     return (
         <div className="mt-5 m-5">
-            <a href="/" className="w-50 btn btn-primary">My List</a>
-            <a href="/calendar" className="w-50 btn btn-primary">My Calendar</a>
+            <Link to="/" className="w-50 btn btn-primary">My List</Link>
+            <Link to="/calendar" className="w-50 btn btn-primary">My Calendar</Link>
         </div>
     )
 }
